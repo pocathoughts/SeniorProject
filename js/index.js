9e0f2e0f300782631ee9e4db55cca1d458b2b6c4
Kevin Marin <kmarin@ufl.edu>
2017-03-30 16:09:09 -0400

    Got login working with christines page. The formatting maaaay be a little fucked because of moving the button out of the form.

--- js/index.js
+++ js/index.js
@@ -17,76 +17,76 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        console.log('Received Device Ready Event');
-        console.log('calling setup push');
-        app.setupPush();
-    },
-    setupPush: function() {
-        console.log('calling push init');
-        var push = PushNotification.init({
-            "android": {
-                "senderID": "XXXXXXXX"
-            },
-            "browser": {},
-            "ios": {
-                "sound": true,
-                "vibration": true,
-                "badge": true
-            },
-            "windows": {}
-        });
-        console.log('after init');
+// var app = {
+//     // Application Constructor
+//     initialize: function() {
+//         this.bindEvents();
+//     },
+//     // Bind Event Listeners
+//     //
+//     // Bind any events that are required on startup. Common events are:
+//     // 'load', 'deviceready', 'offline', and 'online'.
+//     bindEvents: function() {
+//         document.addEventListener('deviceready', this.onDeviceReady, false);
+//     },
+//     // deviceready Event Handler
+//     //
+//     // The scope of 'this' is the event. In order to call the 'receivedEvent'
+//     // function, we must explicitly call 'app.receivedEvent(...);'
+//     onDeviceReady: function() {
+//         console.log('Received Device Ready Event');
+//         console.log('calling setup push');
+//         app.setupPush();
+//     },
+//     setupPush: function() {
+//         console.log('calling push init');
+//         var push = PushNotification.init({
+//             "android": {
+//                 "senderID": "XXXXXXXX"
+//             },
+//             "browser": {},
+//             "ios": {
+//                 "sound": true,
+//                 "vibration": true,
+//                 "badge": true
+//             },
+//             "windows": {}
+//         });
+//         console.log('after init');
 
-        push.on('registration', function(data) {
-            console.log('registration event: ' + data.registrationId);
+//         push.on('registration', function(data) {
+//             console.log('registration event: ' + data.registrationId);
 
-            var oldRegId = localStorage.getItem('registrationId');
-            if (oldRegId !== data.registrationId) {
-                // Save new registration ID
-                localStorage.setItem('registrationId', data.registrationId);
-                // Post registrationId to your app server as the value has changed
-            }
+//             var oldRegId = localStorage.getItem('registrationId');
+//             if (oldRegId !== data.registrationId) {
+//                 // Save new registration ID
+//                 localStorage.setItem('registrationId', data.registrationId);
+//                 // Post registrationId to your app server as the value has changed
+//             }
 
-            var parentElement = document.getElementById('registration');
-            var listeningElement = parentElement.querySelector('.waiting');
-            var receivedElement = parentElement.querySelector('.received');
+//             var parentElement = document.getElementById('registration');
+//             var listeningElement = parentElement.querySelector('.waiting');
+//             var receivedElement = parentElement.querySelector('.received');
 
-            listeningElement.setAttribute('style', 'display:none;');
-            receivedElement.setAttribute('style', 'display:block;');
-        });
+//             listeningElement.setAttribute('style', 'display:none;');
+//             receivedElement.setAttribute('style', 'display:block;');
+//         });
 
-        push.on('error', function(e) {
-            console.log("push error = " + e.message);
-        });
+//         push.on('error', function(e) {
+//             console.log("push error = " + e.message);
+//         });
 
-        push.on('notification', function(data) {
-            console.log('notification event');
-            navigator.notification.alert(
-                data.message,         // message
-                null,                 // callback
-                data.title,           // title
-                'Ok'                  // buttonName
-            );
-       });
-    }
-};
+//         push.on('notification', function(data) {
+//             console.log('notification event');
+//             navigator.notification.alert(
+//                 data.message,         // message
+//                 null,                 // callback
+//                 data.title,           // title
+//                 'Ok'                  // buttonName
+//             );
+//        });
+//     }
+// };
 
 
 
@@ -114,19 +114,23 @@ function login(){
       alert(str);
       return;
     }
-    var idString = userEmail.toLowerCase()  + "ID";
-    var node = document.getElementById(idString);
-    if (node == null){
-      var textNode = document.createElement("p");
-      //textNode.id = idSting;
-      textNode.setAttribute('id', idString);
-      textNode.innerHTML = "User: " + userEmail.toLowerCase()  + " session: " + JSON.stringify(results.session_id);
-      document.getElementById("Sessions").appendChild(textNode);
-    } else {
-      node.innerHTML = "User: " + userEmail.toLowerCase()  + " session: " + JSON.stringify(results.session_id);
-    }
+
+    var str = "User Email" + userEmail.toLowerCase();
+    str += "\nSession_id = " + JSON.stringify(results.session_id);
+    alert(str);
+    // var idString = userEmail.toLowerCase()  + "ID";
+    // var node = document.getElementById(idString);
+    // if (node == null){
+    //   var textNode = document.createElement("p");
+    //   //textNode.id = idSting;
+    //   textNode.setAttribute('id', idString);
+    //   textNode.innerHTML = "User: " + userEmail.toLowerCase()  + " session: " + JSON.stringify(results.session_id);
+    //   document.getElementById("Sessions").appendChild(textNode);
+    // } else {
+    //   node.innerHTML = "User: " + userEmail.toLowerCase()  + " session: " + JSON.stringify(results.session_id);
+    // }
   })
-  .fail(function (data, status) {
+  .fail(function (xhr, data, status) {
     alert( "errorr");
   });
 }
