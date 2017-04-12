983f43dd1190f1fc1d1bf2949e0920287f547c91
aaron silcott <aaron silcott>
2017-04-12 17:27:06 -0400

    community service database

--- DatabaseFiles/ResetDatabase.sql
+++ DatabaseFiles/ResetDatabase.sql
@@ -2,8 +2,8 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 DROP DATABASE LoginSys;
 CREATE DATABASE LoginSys;
-source AccountSystemCreation.sql;
+/*source AccountSystemCreation.sql;
 source CommunityServiceCreation.sql;
 source DatabasePopulation.sql;
-
+*/
 SET FOREIGN_KEY_CHECKS = 1;
